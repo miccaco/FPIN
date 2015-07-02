3359914f433e894ce4e7f1011a763f8c2f54f0f5
miccaco <rodrigo@micca.com.br>
2015-07-02 01:57:50 -0300

    Pasta

--- php/naoAutenticado.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require('verSessao.php');
-	if(!verSessao()){
-		echo '<meta http-equiv="refresh" content="0; url=../index.php">';
-	}
-?>
--- php/titulo_especialidade.php
+++
@@ -1,7 +0,0 @@
-
-    <div class="grid_16" id="content">
-    <!--  TITLE START  -->  <div class="grid_9">
-    <h1 class="content_edit">Cadastra Especialidade</h1>
-    </div>
-    <div class="grid_15" id="textcontent">
-    
